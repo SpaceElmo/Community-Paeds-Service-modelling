1b5ba66ad6f97728f0d26ebb2b5320679fed898b
Raman Sharma <raman0503@hotmail.com>
2025-06-08 23:02:23 +0100

    addded dict of params to extract args. Added plotly to each exploration. Added exploration f f/u and new appt num per clinic

--- model_code.py
+++ model_code.py
@@ -11,7 +11,7 @@ import plotly.graph_objects as go
 
 
 
-'''This code is a model of all non statory work to determine wait times dependant on referral numbers and age distributions. Capacity is calculated annually as it scales linearly with time similarly to demand. '''
+'''This code is a model of all non statory work to determine wait times dependant on referral numbers and age distributions as well as diagnostic distributions. Capacity is calculated annually as it scales linearly with time similarly to demand. '''
 
 ##Global default Variables. diagnosis rates for each age group <6 and >6 were calculated form the 24/25 referral sheet. This covered 88% of referrals with the remainder being referrrals##
 #referral rate
@@ -19,8 +19,8 @@ Total_ref_num=700
 num_of_years=1
 
 #staffing of service WTE
-num_docs=4 #WTE doctors
-num_nurses=3 #WTE nurses
+num_docs=5.1 #WTE doctors for comm paeds
+num_nurses=2.4 #WTE nurses for comm paeds
 
 #provide appt numbers and types at a service or individual level to initiate the fu and new capacity per clinic variables
 total_clinic_num_doc=4#total number of non-staturory clinics over a set period for the doctors. Can be a week or month
@@ -29,14 +29,14 @@ total_fu_appt_doc=20#total number of new appts over a set period for the doctors
 new_per_clinic_doc= total_new_appt_doc/total_clinic_num_doc#no new patient appt per clinic
 fu_per_clinic_doc= total_fu_appt_doc/total_clinic_num_doc #no of f/u appt per clinic
 total_clinic_num_nurse=3#total number of non-staturory clinics over a set period for the nurses. Can be a week or month
-total_new_appt_nurse=0.2#total number of new appts over a set period for the nurses. Can be a week or month. Must be same as total clinic num
+total_new_appt_nurse=0.1#total number of new appts over a set period for the nurses. Can be a week or month. Must be same as total clinic num
 total_fu_appt_nurse=12#total number of new appts over a set period for the nurses. Can be a week or month. Must be same as total clinic num
 new_per_clinic_nurse= total_new_appt_nurse/total_clinic_num_nurse#no new patient appt per clinic
 fu_per_clinic_nurse= total_fu_appt_nurse/total_clinic_num_nurse #no of f/u appt per clinic
 
 #provide number of non statutory clinics that service provides in a week per clinician
-num_of_clinics_docs = 4#per week per doc
-num_of_clinics_nurses = 3#per week per nurse
+num_of_clinics_docs = 3.5#per week per WTE doc
+num_of_clinics_nurses = 3.5#per week per WTE nurse
 
 #provide the diagnostic dependant fu burden. This encompasses the type of service model (1 stop shop, streamed) as well as discharge rates post assessment. ie not all ADHDs need f/u 
 ADHD_fu_num=2#number of follow ups always needed post new assessment
@@ -46,29 +46,31 @@ ASD_reg_fu_num=0.2#number of follow ups needed per year normally
 Complex_fu_num=3.5#number of follow ups always needed post new assessment
 Complex_reg_fu_num=1.5#number of follow ups needed per year normally
 min_age=1.5
-max_age=17
+max_age=17.5
 fu_DNA_rate=0.05
 new_DNA_rate=0.01
 
-free_var_list=[Total_ref_num,num_of_years,
-num_docs,
-num_nurses,
-new_per_clinic_doc, #length of time of new patient appt
-fu_per_clinic_doc, #length of time of f/u appt
-new_per_clinic_nurse,
-fu_per_clinic_nurse,
-num_of_clinics_docs,
-num_of_clinics_nurses,
-ADHD_fu_num,
-ADHD_reg_fu_num,
-ASD_fu_num,
-ASD_reg_fu_num,
-Complex_fu_num,
-Complex_reg_fu_num,
-min_age,
-max_age]
+default_vals={'Total_ref_num':Total_ref_num,'num_of_years':num_of_years,
+'num_docs':num_docs,
+'num_nurses':num_nurses,
+'new_per_clinic_doc':new_per_clinic_doc, #length of time of new patient appt
+'fu_per_clinic_doc':fu_per_clinic_doc, #length of time of f/u appt
+'new_per_clinic_nurse':new_per_clinic_nurse,
+'fu_per_clinic_nurse':fu_per_clinic_nurse,
+'num_of_clinics_docs':num_of_clinics_docs,
+'num_of_clinics_nurses':num_of_clinics_nurses,
+'ADHD_fu_num':ADHD_fu_num,
+'ADHD_reg_fu_num':ADHD_reg_fu_num,
+'ASD_fu_num':ASD_fu_num,
+'ASD_reg_fu_num':ASD_reg_fu_num,
+'Complex_fu_num':Complex_fu_num,
+'Complex_reg_fu_num':Complex_reg_fu_num,
+'min_age':min_age,
+'max_age':max_age,
+'fu_DNA_rate':fu_DNA_rate,
+'new_DNA_rate':new_DNA_rate}
 
-#Hard coded variables in functions(cannot alter them here)
+#Hard coded variables in functions(cannot alter them here. These were derived from 23/24 referral data)
 '''Working_weeks=42
 ASD_frac_under6=0.74
 ADHD_frac_under6= 0.04
@@ -154,8 +156,8 @@ def new_capacity_nurses(num_nurses,num_of_clinics_nurses,new_per_clinic_nurse,Wo
 def get_fu_demand(mean_annual_fu_burden,sd_annual_fu_burden,annual_new_burden):
     '''gets the annual follow up burden from the calculated ratio of new to f/u'''
     fu_demand_mean=(mean_annual_fu_burden)*annual_new_burden
-    fu_demand_min=(mean_annual_fu_burden-sd_annual_fu_burden)*annual_new_burden#using the sd of fu burden as a proxy as it takes into account diagnosis and age
-    fu_demand_max=(mean_annual_fu_burden+sd_annual_fu_burden)*annual_new_burden
+    fu_demand_min=(mean_annual_fu_burden-np.sqrt(sd_annual_fu_burden))*annual_new_burden#using the sd of fu burden as a proxy as it takes into account diagnosis and age
+    fu_demand_max=(mean_annual_fu_burden+np.sqrt(sd_annual_fu_burden))*annual_new_burden
     return(fu_demand_max,fu_demand_min,fu_demand_mean)
 
 def get_wait_times(fu_demand_min,fu_demand_max,fu_demand_mean,new_demand,fu_capacity,new_capacity):
@@ -165,9 +167,10 @@ def get_wait_times(fu_demand_min,fu_demand_max,fu_demand_mean,new_demand,fu_capa
     #print(f'The lower bound wait_time using a service workforce of {num_docs} docs and {num_nurses} nurses is {min_wait_time*12} months. The upper bound wait time is {max_wait_time*12} months')
     return(min_wait_time,max_wait_time,mean_wait_time)
 
-def run_sim(Total_ref_num,num_docs,num_nurses):
-    '''runs the sim and allows the local variables in teh argument to vary'''
-    age_dist=find_age_dist(Total_ref_num,min_age,max_age,plot=True)
+def run_sim(Total_ref_num,num_of_years,num_docs,num_nurses,new_per_clinic_doc,fu_per_clinic_doc,new_per_clinic_nurse,fu_per_clinic_nurse,num_of_clinics_docs,num_of_clinics_nurses,
+            ADHD_fu_num,ADHD_reg_fu_num,ASD_fu_num,ASD_reg_fu_num,Complex_fu_num,Complex_reg_fu_num,min_age,max_age,fu_DNA_rate,new_DNA_rate):
+    '''runs the sim and allows the local variables in the argument to vary. Uses a dict of default vals'''
+    age_dist=find_age_dist(Total_ref_num,min_age,max_age,plot=False)
     total_fu=get_ADHD_fu_burden(age_dist,ADHD_fu_num,ADHD_reg_fu_num)+get_ASD_fu_burden(age_dist,ASD_fu_num,ASD_reg_fu_num)+get_Complex_fu_burden(age_dist,Complex_fu_num,Complex_reg_fu_num)
     mean_annual_fu,sd_annual_fu,upper_lim,lower_lim=get_annual_fu_burdens(age_dist,Total_ref_num,total_fu,fu_DNA_rate)
     #print(f"the ideal ratio of new to follow up is 1:{int(upper_lim)} and minimum safe is 1:{int(lower_lim)}")
@@ -181,8 +184,9 @@ def run_sim(Total_ref_num,num_docs,num_nurses):
     return(results)
 
 #########execute###########################################################################################################
-explore_workforce=True
+explore_workforce=False
 explore_referral_rate=False#If both are false then a single service model is created
+explore_models=True
 if explore_workforce and explore_referral_rate:
     print('Do not run both explore workforce and referral rate')
     raise ValueError
@@ -191,21 +195,49 @@ if explore_workforce and explore_referral_rate:
 #run the code with default vals but vary a line depending on range
 num_docs_range=np.arange(2,9,1)
 num_nurses_range=np.arange(0,7,1)
-ref_rate_range=np.arange(300,900,100)
+ref_rate_range=np.arange(300,950,50)
+new_appt_type_range=np.arange(1,5,1)
+fu_appt_type_range=np.arange(0,9,1)
 
 if explore_referral_rate:
     results=np.empty((0,6))
     for ref_num in ref_rate_range:
-        result=run_sim(ref_num,num_docs,num_nurses)
+        params={**default_vals,'Total_ref_num':ref_num}
+        result=run_sim(**params)
         results=np.vstack((results,result))
     for i,val in enumerate(results[:,3]):
-        print(f"At a referral rate of {val}, the mean wait time is {results[i,2]} months with upper bound {results[i,1]} and lower bound {results[i,0]} months ' ")    
-       
+        print(f"At a referral rate of {val}, the mean wait time is {results[i,2]} months with upper bound {results[i,1]} and lower bound {results[i,0]} months ' ")        
+    plot_2d=True    
+    if plot_2d:
+        docs = results[:, 4]  
+        nurses = results[:, 5]  
+        min_time = results[:, 0]
+        max_time = results[:, 1] 
+        mean_time= results[:, 2]
+        ref_rate=results[:,3]
+        fig,ax=plt.subplots()
+        ax.errorbar(ref_rate,mean_time,yerr=[min_time,max_time],fmt='o',capsize=5, capthick=1, alpha=0.5,color='blue', label='Wait time with referral rate')
+        ax.set_xlabel("Referral Rate")
+        ax.set_ylabel("Wait time (months)")
+        ax.set_title("Wait time for current workforce at different referral rates")
+        ax.axhline(y=12, color="red", linestyle="--", linewidth=1, label="12-Month Marker")
+
+        ax.legend()
+
+# Show the plot
+        fig.show()
+        fig.savefig("Ref_rate_plot.png", dpi=800, bbox_inches="tight")
+
+
+
+
+#explore the workforce range and plot        
 elif explore_workforce:
     results=np.empty((0,6))
     for i in num_docs_range:
         for j in num_nurses_range:
-            result=run_sim(Total_ref_num,num_docs=i,num_nurses=j)
+            params={**default_vals,'num_docs':i,'num_nurses':j}
+            result=run_sim(**params)
             results=np.vstack((results,result))           
     targ_wait=12# target wait time months
    # print('all results',results)
@@ -217,46 +249,131 @@ elif explore_workforce:
         print(f'for your service with an annual referral rate of {filtered_results[min_idx,3]} a minimum workforce of {filtered_results[min_idx,4]} WTE doctors and {filtered_results[min_idx,5]} WTE nurses would keep mean wait time less than {targ_wait} months')
     else:
         print('No work force combination in that range can meet your wait target. extend the range or increase the target difference')
+    plot_3d=True    
+    if plot_3d:
+        docs = results[:, 4]  
+        nurses = results[:, 5]  
+        min_time = results[:, 0]
+        max_time = results[:, 1] 
+        mean_time= results[:, 2]
+        ref_rate=results[:,3]
+        # Create plot
+        fig = plt.figure(figsize=(10, 8))
+        ax = fig.add_subplot(projection='3d')  # 3D plot
+
+    
+
+        # Define grid for the plane
+        x_range = np.linspace(np.min(docs), np.max(docs),30)  # Adjust based on your data range
+        y_range = np.linspace(np.min(nurses), np.max(nurses), 30)
+        X, Y = np.meshgrid(x_range, y_range)
+
+        Z = np.full_like(X, 12)  # Creates a flat plane at z = 12
+        Z1= griddata((docs,nurses),mean_time,(X,Y),method='cubic')
+        Z2= griddata((docs,nurses),min_time,(X,Y),method='cubic')
+        Z3= griddata((docs,nurses),max_time,(X,Y),method='cubic')
+        
+            # Create the plotly surface objects
+        fig = go.Figure()
+
+        fig.add_trace(go.Surface(x=X, y=Y, z=Z2, colorscale=[[0,'Red'],[1,'Red']], opacity=0.1, name='Lower Bound',showscale=False))
+        fig.add_trace(go.Surface(x=X, y=Y, z=Z1, colorscale='Blues_r', opacity=1, name='Mean Time',showscale=True,colorbar=dict(title="Wait Time (months)")
+    ))
+        fig.add_trace(go.Surface(x=X, y=Y, z=Z3, colorscale=[[0,'Red'],[1,'Red']], opacity=0.1, name='Upper Bound',showscale=False))
+        fig.add_trace(go.Surface(x=X, y=Y, z=Z, colorscale=[[0,'Gray'],[1,'Gray']], opacity=0.4, name='12 month boundary',showscale=False))
+
+        # Update layout
+        fig.update_layout(
+            title='Interpolated Wait Times for different workforce compositions',
+            scene=dict(
+                xaxis_title='Doctors',
+                yaxis_title='Nurses',
+                zaxis_title='Wait Time (months)'
+            ),
+            width=900,
+            height=900
+        )
+        plt.show()
+        fig.write_html("workforceplot.html")    
             
+elif explore_models:
+    #adjusting doctors fu to new ratio only
+    results=np.empty((0,8))
+    for i in new_appt_type_range:
+        for j in fu_appt_type_range:
+            params={**default_vals,'fu_per_clinic_doc':j,'new_per_clinic_doc':i}
+            result=run_sim(**params)
+            result=np.append(result,[i,j]).reshape(1,8)#add the new and fu type num
+            results=np.vstack((results,result))
+    #print(results)        
+    plot_3d=True    
+    if plot_3d:
+        docs = results[:, 4]  
+        nurses = results[:, 5]  
+        min_time = results[:, 0]
+        max_time = results[:, 1] 
+        mean_time= results[:, 2]
+        ref_rate=results[:,3]
+        new_appts = results[:,6]
+        fu_appts= results[:,7]
+        #print(new_appts,fu_appts)
+        # Create plot
+        fig = plt.figure(figsize=(10, 8))
+        ax = fig.add_subplot(projection='3d')  # 3D plot
+
+    
+
+        # Define grid for the plane
+        x_range = np.linspace(np.min(new_appts), np.max(new_appts),50)  # Adjust based on your data range
+        y_range = np.linspace(np.min(fu_appts), np.max(fu_appts), 50)
+        #print(x_range,y_range)
+        X, Y = np.meshgrid(x_range, y_range)
+
+        Z = np.full_like(X, 12)  # Creates a flat plane at z = 12
+        Z1= griddata((new_appts,fu_appts),mean_time,(X,Y),method='linear')
+        Z2= griddata((new_appts,fu_appts),min_time,(X,Y),method='linear')
+        Z3= griddata((new_appts,fu_appts),max_time,(X,Y),method='linear')
+        #print(Z,Z1,Z2,Z3)
+        
+            # Create the plotly surface objects
+        fig = go.Figure()
+
+        fig.add_trace(go.Surface(x=X, y=Y, z=Z2, colorscale=[[0,'Red'],[1,'Red']], opacity=0.1, name='Lower Bound',showscale=False))
+        fig.add_trace(go.Surface(x=X, y=Y, z=Z1, colorscale='Blues_r', opacity=1, name='Mean Time',showscale=True,colorbar=dict(title="Wait Time (months)")))
+        fig.add_trace(go.Surface(x=X, y=Y, z=Z3, colorscale=[[0,'Red'],[1,'Red']], opacity=0.1, name='Upper Bound',showscale=False))
+        fig.add_trace(go.Surface(x=X, y=Y, z=Z, colorscale=[[0,'Gray'],[1,'Gray']], opacity=0.4, name='12 month boundary',showscale=False))
+
+        # Update layout
+        fig.update_layout(
+            title='Interpolated Wait Times for different new and f/u compositions for doctors',
+            scene=dict(
+                xaxis_title='New appt per clinic',
+                yaxis_title='Follow up appts per clinic',
+                zaxis_title='Wait Time (months)'
+            ),
+            width=900,
+            height=900
+        )
+        plt.show()
+        fig.write_html("service_model.html")    
+
 
 else:
     results=np.empty((0,6))
-    result=run_sim(Total_ref_num,num_docs,num_nurses)
+    result=run_sim(**default_vals)
     results=np.vstack((results,result))
     print(f'for your service with an annual referral rate of {results[0,3]} the mean wait time is {results[0,2]} months with upper bound {results[0,1]} and lower bound {results[0,0]} months ')
 #print(results.shape)
 #print(results)
 
-plot_3d=True
-plot_2d=False
-# Extract columns
-docs = results[:, 4]  # First independent variable
-nurses = results[:, 5]  # Second independent variable
-min_time = results[:, 0] # Dependent variable (third column)
-max_time = results[:, 1] # Dependent variable (fourth column)
-mean_time= results[:, 2]
-ref_rate=results[:,3]
-if plot_3d:
-    # Create plot
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(projection='3d')  # 3D plot
 
-    # Plot scatters
-    #ax.scatter(docs, nurses, min_time, color='red', alpha=0.2, s=1 ,label="Lower bound wait time")
-    #ax.scatter(docs, nurses, mean_time, color='blue', alpha=0.6, s=3, label="Mean wait time")
-    #ax.scatter(docs, nurses, max_time, color='red', alpha=0.2,s=1, label="Upper bound wait time")
 
-    # Define grid for the plane
-    x_range = np.linspace(np.min(docs), np.max(docs),30)  # Adjust based on your data range
-    y_range = np.linspace(np.min(nurses), np.max(nurses), 30)
-    X, Y = np.meshgrid(x_range, y_range)
 
-    Z = np.full_like(X, 12)  # Creates a flat plane at z = 12
-    Z1= griddata((docs,nurses),mean_time,(X,Y),method='cubic')
-    Z2= griddata((docs,nurses),min_time,(X,Y),method='cubic')
-    Z3= griddata((docs,nurses),max_time,(X,Y),method='cubic')
-    
-    '''
+
+    '''# Plot scatters Matplotlib
+    ax.scatter(docs, nurses, min_time, color='red', alpha=0.2, s=1 ,label="Lower bound wait time")
+    ax.scatter(docs, nurses, mean_time, color='blue', alpha=0.6, s=3, label="Mean wait time")
+    ax.scatter(docs, nurses, max_time, color='red', alpha=0.2,s=1, label="Upper bound wait time")
     ax.plot_wireframe(X, Y, Z, alpha=0.3, color='gray')  # Semi-transparent gray plane
     ax.plot_surface(X, Y, Z1, alpha=0.8, color='blue')
     ax.plot_wireframe(X, Y, Z2, alpha=0.5, color='red')
@@ -269,32 +386,9 @@ if plot_3d:
     ax.legend()
     plt.show()
 '''
-    # Create the plotly surface objects
-    fig = go.Figure()
 
-    fig.add_trace(go.Surface(x=X, y=Y, z=Z2, colorscale=[[0,'Red'],[1,'Red']], opacity=0.1, name='Lower Bound',showscale=False))
-    fig.add_trace(go.Surface(x=X, y=Y, z=Z1, colorscale='Blues_r', opacity=1, name='Mean Time',showscale=True,colorbar=dict(title="Wait Time (months)")
-))
-    fig.add_trace(go.Surface(x=X, y=Y, z=Z3, colorscale=[[0,'Red'],[1,'Red']], opacity=0.1, name='Upper Bound',showscale=False))
-    fig.add_trace(go.Surface(x=X, y=Y, z=Z, colorscale=[[0,'Gray'],[1,'Gray']], opacity=0.4, name='12 month boundary',showscale=False))
-
-    # Update layout
-    fig.update_layout(
-        title='Interpolated Wait Times',
-        scene=dict(
-            xaxis_title='Doctors',
-            yaxis_title='Nurses',
-            zaxis_title='Wait Time'
-        ),
-        width=900,
-        height=900
-    )
-    fig.show()
-    fig.write_html("workforceplot.html")
-
-
-
-
+plot_2d=False
+# Extract columns
 
 
 if plot_2d:
